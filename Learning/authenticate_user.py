be27dd6c4fc64e38c908da4475f3bc04fdaa5345
shaliniaiitd <shalinia.iitd@gmail.com>
2023-03-27 23:12:50 +0530

    Learning API testing

---
+++ Learning/authenticate_user.py
@@ -0,0 +1,8 @@
+import requests
+
+resp = requests.get("https://the-internet.herokuapp.com/basic_auth", auth = ("admin", "admin") )
+
+print(resp) #200
+
+resp = requests.get("https://the-internet.herokuapp.com/basic_auth", auth = ("admin", "admn") )
+print(resp) #401 - user unauthorised
